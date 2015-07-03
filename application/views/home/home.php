756f188a7dc7003d87099c1e0d47f8d0a18c00bb
tocurd <1137716847@qq.com>
2015-07-03 16:45:44 +0800

    index
    首页页面布局

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,27 +1,51 @@
 <div class="main">
 	<div class="wrapper">
+	
+	
 		<div class="slider">
 			<div class="left-slider">
 				<ul>
-					<li><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLAwcDOP64mFcXsv8w5xjA2EdQc3nmQjnB40jVPficcDwHuGevRBgB1ibMv85w6yYqKWg/440" /></li>
-					<li><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLDhYZtZ3ZxS9lZ7OZlvj2rJjWIbFEh4QEeSWhUz3mH0qjs409OG63C8cAhTnoE5mzc/220" /></li>
-					<li><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLCGGZIM1E47wcibVBmC7JqmDLkxjZW5B4zfvyBHlezcsfcO34Vryf3qylECNABXp4pA/220" /></li>
+					<li><i style="display:none"></i><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLAwcDOP64mFcXsv8w5xjA2EdQc3nmQjnB40jVPficcDwHuGevRBgB1ibMv85w6yYqKWg/440" /></li>
+					<li><i></i><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLDhYZtZ3ZxS9lZ7OZlvj2rJjWIbFEh4QEeSWhUz3mH0qjs409OG63C8cAhTnoE5mzc/440" /></li>
+					<li><i></i><img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLCGGZIM1E47wcibVBmC7JqmDLkxjZW5B4zfvyBHlezcsfcO34Vryf3qylECNABXp4pA/440" /></li>
 				</ul>
 			</div>
 			<div class="center-slider">
-				<img src="static/image/test.jpg" />
+				<img src="http://10.url.cn/qqcourse_logo_rn/ajNVdqHZLLAwcDOP64mFcXsv8w5xjA2EdQc3nmQjnB40jVPficcDwHuGevRBgB1ibMv85w6yYqKWg/440" />
 			</div>
 			<div class="right-slider">
-				<div class="slider-content">
-					content
+				<div class="right-data" style="display:block">
+					<div class="slider-content">
+						课程1
+					</div>
+					<div class="slider-content">
+						课表1
+					</div>
 				</div>
-				<div class="slider-content">
-					content
+				
+				<div class="right-data">
+					<div class="slider-content">
+						课程2
+					</div>
+					<div class="slider-content">
+						课表2
+					</div>
 				</div>
+				
+				<div class="right-data">
+					<div class="slider-content">
+						课程3
+					</div>
+					<div class="slider-content">
+						课表3
+					</div>
+				</div>
+				
+				
 			</div>
-			
-		
 		</div>
+		
+		
 		<div class="tab">
 			<ul>
 				<li class="hover"><a href="javascript:void(0)">Unity-3D</a></li>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,15 +3,17 @@
 <head>
 	<meta charset="utf-8">
 	<title>天地培训官网首页_游戏开发培训_手机游戏开发培训_游戏编程培训_游戏策划培训</title>
-	<link rel="Shortcut Icon" href="favicon.ico">
-	<link rel="stylesheet" href="static\home.css">
+	<link rel="Shortcut Icon" href="./favicon.ico">
+	<link rel="stylesheet" href="./static/home.css">
+	<script src="./static/js/jquery-2.1.4.min.js"></script>
+	<script src="./static/js/tiandi.min.js?var=201507031350"></script>
 	<meta name="keywords" content="游戏开发培训,手机游戏开发培训,游戏编程培训,游戏策划培训,天地培训">
 	<meta name="description" content="天地培训是国内从事手机、网页游戏开发唯一专业的培训机构，Adobe认证培训中心， 天地培训起源于全球最大的网页游戏开发者学习交流社区9RIA.com天地会，携手众多知名讲师为您带来精彩社交游戏开发课程">
 </head>
 <body>
 	<div class="header">
 		<div class="wrapper header-nav">
-			<a href="index.php"><img class="logo" src="static/image/logo.png" height="" width="" alt="天地培训"></a>
+			<a href="./"><img class="logo" src="static/image/logo.png" height="" width="" alt="天地培训"></a>
 			<ul class="nav">
 				<li><a href="">在线课堂</a></li>
 				<li><a href="">实时答疑</a></li>
