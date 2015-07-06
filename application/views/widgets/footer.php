1600670ac66e982e2167ab0a037df125dae8c9ed
PoppinL <poppinlp@gmail.com>
2015-07-06 22:37:04 +0800

    jquery区分浏览器
    auto_load 加入 ua，jQuery 根据浏览器提供不同版本

--- application/views/widgets/footer.php
+++ application/views/widgets/footer.php
@@ -1,3 +1,15 @@
 <div class="footer">
 </div>
-<script src="./static/js/jquery-2.1.4.min.js"></script>
+
+<?php
+switch($this->agent->browser()) {
+    case 'Opera':
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+        echo '<script src="./static/js/jquery-2.1.4.min.js"></script>';
+        break;
+    default:
+        echo '<script src="./static/js/jquery-1.11.3.min.js"></script>';
+        break;
+}
