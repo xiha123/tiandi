6abbc2b777085f9bb4eb44d6b1c9aaac81982ea6
PoppinL <poppinlp@gmail.com>
2015-07-17 00:01:55 +0800

    update admin login page; rm admin fa
    update admin login page; rm admin fa

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>天地君道后台登陆系统</title>
- <link rel="stylesheet" href="./static/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="./static/css/admin/reset.css"/>
-    <link rel="stylesheet" href="./static/css/admin/land_main.css"/>
+<?php $this->load->view('widgets/admin/header.php'); ?>
+<link rel="stylesheet" href="./static/css/admin/login.css">
 </head>
 
 <body>
-
-	<div class="box">
-        <img src="./static/image/admin/logo-hd.png" class="logo fl" alt="天地君道" width="220" height="162"/>
-                <form action="admin" method="post">
-                    <input type="text"  placeholder="登录名" name="username"/>
-                    <input type="password"  placeholder="密码" name="password"/>
-
-                    <ul class="msg">
-                       <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>请输入登陆密码</li>
-                        <li class="tishi">  <i class="icon-exclamation-sign" target="_blank"></i>请输入登陆名</li>
-                        <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>登录名或密码错误</li>
-                        <li><input type="submit" class="landing" value="登录" /></li>
-
-                    </ul>
-                </form>
+    <div class="box">
+        <img src="./static/image/admin/logo-hd.png" class="logo" alt="天地君道" width="220" height="162"/>
+        <form action="admin" method="post">
+            <input type="text"  placeholder="登录名" name="username"/>
+            <input type="password"  placeholder="密码" name="password"/>
+            <ul class="msg">
+                <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>请输入登录密码</li>
+                <li class="tishi">  <i class="icon-exclamation-sign" target="_blank"></i>请输入登录名</li>
+                <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>登录名或密码错误</li>
+            </ul>
+            <input type="submit" class="login-btn" value="登录" />
+        </form>
     </div>
 </body>
 </html>
--- application/views/widgets/admin/header.php
+++ application/views/widgets/admin/header.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype HTML>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <title>天地君道后台管理系统</title>
-<link rel="stylesheet" href="../static/lib/bootstrap/bootstrap.min.css">
-<link rel="stylesheet" href="../static/css/admin/global.css" type="text/css" />
-<link rel="stylesheet" href="../static/lib/fa/css/font-awesome.min.css" type="text/css" />
+<link rel="stylesheet" href="./static/lib/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="./static/css/admin/global.css">
+<link rel="stylesheet" href="./static/lib/fa/css/font-awesome.min.css">
