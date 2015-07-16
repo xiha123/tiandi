0882a169243515e042f58eb717aa9b49dc47ea86
tocurd <1137716847@qq.com>
2015-07-16 10:18:22 +0800

    admin login 细节调整
    增加了一个高清的LOGO，修改了部分细节

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -3,23 +3,23 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>天地君道后台登陆系统</title>
- <link rel="stylesheet" href="./static/css/admin/font-awesome-4.3.0/css/font-awesome.min.css"/>
+ <link rel="stylesheet" href="./static/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="./static/css/admin/reset.css"/>
     <link rel="stylesheet" href="./static/css/admin/land_main.css"/>
 </head>
 
 <body>
-{blog_title}
+
 	<div class="box">
-        <img src="./static/image/admin/logo.png" class="logo fl" alt="天地君道" width="220" height="162"/>
+        <img src="./static/image/admin/logo-hd.png" class="logo fl" alt="天地君道" width="220" height="162"/>
                 <form action="admin" method="post">
                     <input type="text"  placeholder="登录名" name="username"/>
                     <input type="password"  placeholder="密码" name="password"/>
 
                     <ul class="msg">
-                       <li><i class="fa fa-exclamation-circle msg" target="_blank"></i>请输入登陆密码</li>
-                        <li>  <i class="fa fa-exclamation-circle msg" target="_blank"></i>请输入登陆名</li>
-                        <li><i class="fa fa-exclamation-circle msg" target="_blank"></i>登录名或密码错误</li>
+                       <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>请输入登陆密码</li>
+                        <li class="tishi">  <i class="icon-exclamation-sign" target="_blank"></i>请输入登陆名</li>
+                        <li class="tishi"><i class="icon-exclamation-sign" target="_blank"></i>登录名或密码错误</li>
                         <li><input type="submit" class="landing" value="登录" /></li>
 
                     </ul>
