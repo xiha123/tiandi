f5aea7d2c0430a8989146b5a3ba949f353d8c687
tocurd <1137716847@qq.com>
2015-07-16 10:46:23 +0800

    后台分页样式

--- application/views/admin/slider.php
+++ application/views/admin/slider.php
@@ -35,8 +35,19 @@
 					<tr><td>css3 calc():css简单的数学运算-加减乘除 | HTML老虎</td><td>点击浏览</td><td>2015年7月14日15:19:22</td><td>测试性质的添加测试</td><td>正常</td><td><i class="icon-edit edit-slider"></i><i class="icon-trash remove-slider" data-id="0"></i></td></tr>
 					<tr><td>css3 calc():css简单的数学运算-加减乘除 | HTML老虎</td><td>点击浏览</td><td>2015年7月14日15:19:22</td><td>测试性质的添加测试</td><td>正常</td><td><i class="icon-edit edit-slider"></i><i class="icon-trash remove-slider" data-id="0"></i></td></tr>
 				</table>
+				<div class="page">
+					<font>共 0 页，当前正在第 0 页，共有数据 0 条</font>
+					<div class="right">
+						<input type="submit" value="上一页">
+						<form action="#" method="get">
+							<input type="text" placeholder="页数">
+							<input type="submit" value="跳转" style="margin-left:-5px;">
+						</form>
+						<input type="submit" value="下一页">
+					</div>
+				</div>
 			</div>
-
+			
 
 		</div>
 	</div>
