f34e5ec7f8115848f55778a0ba108a205e1e6316
tocurd <1137716847@qq.com>
2015-07-27 17:05:44 +0800

    add seconds home

--- application/views/seconds/home.php
+++ application/views/seconds/home.php
@@ -1,12 +1,162 @@
 <?php $this->load->view('widgets/header.php'); ?>
-	<link rel="stylesheet" href="static/css/course.css">
-</head>
+<link rel="stylesheet" href="./static/css/seconds/home.css">
 <body>
+
+<div class="wrapper">
+	<div class="doubt">
+		<table class="table">
+			<tr><td><input type="text" class="title" placeholder="一句话写下你遇到的问题"></td></tr>
+			<tr><td>
+				<div class="desc">
+					<textarea class="input-content" placeholder="描述你的详细信息"></textarea>
+					<a href="javascript:" class="Language">选择语言</a>
+					<textarea class="code" placeholder="选择编程语言以后，写下你的问题涉及到的代码"></textarea>
+				</div>
+			</td></tr>
+			<tr><td><div class="tag">
+				<span class="tag-box">javascript <button class="close">X</button></span>
+				<span class="tag-box">html5 <button class="close">X</button></span>
+				<input type="text" class="input-tag" placeholder="请输入标签">
+			</div></td></tr>
+		</table>
+	</div>
+	
+	<div class="content">
+		<div class="left-content">
+			<ul class="title">
+				<li class="active"><a href="javascript:">最新</a></li>
+				<li><a href="javascript:">热门</a></li>
+				<li><a href="javascript:">众筹</a></li>
+			</ul>
+
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>
+			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>
+			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>
+			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>
+			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>			
+			<ul class="list-data">
+				<li>
+					<div class="link-num"><p>999</p><p>点赞</p></div>
+					<div class="list-title">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+					<ul class="list-tag">
+						<li><a href="#" class="tag-box">html5</a></li>
+						<li><a href="#" class="tag-box">javascript</a></li>
+						<li><a href="#" class="tag-box">jquery</a></li>
+					</ul>
+					<div class="list-date">大神 <font>Tocurd</font> 回答于：20:30</div>
+				</li>
+			</ul>
+		</div>
+		<div class="right-content">
+			<ul class="title">
+				<li ><a href="javascript:">热门标签</a></li>
+			</ul>
+			<ul class="list-tag">
+				<li><a href="#" class="tag-box">html5</a></li>
+				<li><a href="#" class="tag-box">javascript</a></li>
+				<li><a href="#" class="tag-box">jquery</a></li>
+				<li><a href="#" > 更多</a></li>
+			</ul>
+		</div>
+		<div class="page">
+			<ul>
+				<li><a href="javascript:">< 上一页</a></li>
+				<li class="active"><a href="javascript:">1</a></li>
+				<li><a href="javascript:">2</a></li>
+				<li><a href="javascript:">3</a></li>
+				<li><a href="javascript:">下一页 ></a></li>
+			</ul>
+		</div>
+	</div>
+
+
+</div>
 
 
 
 <?php $this->load->view('widgets/footer.php'); ?>
-<!--<script src="./static/js/home.js"></script>-->
-
 </body>
 </html>
