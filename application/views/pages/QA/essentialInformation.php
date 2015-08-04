dc7600ee0c61a903f1a3fa3fe8344a7264223e4e
lovelywing <lovelywing520@gmail.com>
2015-08-04 23:19:50 +0800

    Modified global.css  sociality li{float:left}--del
    Modified global.css  sociality li{float:left}--del

--- application/views/pages/QA/essentialInformation.php
+++ application/views/pages/QA/essentialInformation.php
@@ -1,36 +1,44 @@
 <?php $this->load->view('widgets/header.php'); ?>
-<link rel="stylesheet" href="static/css/QA/##" />
+<link rel="stylesheet" href="static/css/QA/personalInformation.css" />
 
 </head>
 
 <div class="box">
-    <h3>基 础 资 料</h3>
-    <div class="Photo">
-        <!--相片位置后期需修改-->
-    </div>
-    <input type="text" placeholder="昵 称"/>
-    <textarea name="" id="Signature" cols="30" rows="10" placeholder="签名档"></textarea>
-    <h3>账户设置</h3>
-    <input type="text" placeholder="邮箱"/>
-    <input type="text" placeholder="手机号码"/>
-    <h3>账户设置</h3>
-    <input type="password" placeholder="旧密码"/>
-    <input type="password" placeholder="新密码"/>
-    <input type="password" placeholder="确认新密码"/>
-    <h3>绑定第三方账户</h3>
-    <ul class="sociality">
-        <li><i class="fa fa-qq fl"></i></li>
-        <li><i class="fa fa-weibo fl"></i></li>
-    </ul>
-    <button type="submit">
-        提 交<i class="fa fa-arrow-circle-right"></i>
-    </button>
-    <h3>大神资料设置</h3>
-        <p>真 实 姓 名：某某某</p>
-        <p>支付宝账号：某某某</p>
-    <input type="password" placeholder="支付宝账号"/>
-    <textarea name="" id="experience" cols="30" rows="10" placeholder="项目经历"></textarea>
-    <button type="submit">
-        提 交<i class="fa fa-arrow-circle-right"></i>
-    </button>
+    <form action="#" method="get" class="submitbutton">
+        <h3 class="information">基 础 资 料</h3>
+        <div class="Photo"style="background-color:#209ba2;width:150px;height: 150px;margin: 25px auto;">
+        </div>
+        <input type="text" placeholder="昵 称"/>
+        <textarea name="" id="signature" cols="30" rows="10" placeholder="签名档" ></textarea>
+        <h3 class="information">账户设置</h3>
+        <input type="text" placeholder="邮箱"/>
+        <input type="text" placeholder="手机号码"/>
+        <h3 class="information">账户设置</h3>
+
+        <input type="password" placeholder="旧密码"/>
+        <input type="password" placeholder="新密码"/>
+        <input type="password" placeholder="确认新密码"/>
+
+        <h3 class="information">绑定第三方账户</h3>
+        <ul class="sociality">
+            <li><i class="fa fa-qq"></i></li>
+            <li><i class="fa fa-weibo"></i></li>
+        </ul>
+        <button type="submit">
+            提 交<i class="fa fa-arrow-circle-right"></i>
+        </button>
+        <div class="line"></div>
+        <h3 class="information">大神资料设置</h3>
+        <div class="personmessage">
+            <p>真 实 姓 名：某某某</p>
+            <p>支付宝账号：某某某</p>
+        </div>
+
+        <input type="password" placeholder="支付宝账号"/>
+        <textarea name="" id="experience" cols="30" rows="10" placeholder="项目经历"></textarea>
+        <button type="submit">
+            提 交<i class="fa fa-arrow-circle-right"></i>
+        </button>
+    </form>
+
 </div>
