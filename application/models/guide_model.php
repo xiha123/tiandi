39ec350b32d7c660ef0bbcaded3dc1fe1aa08772
PoppinL <poppinlp@gmail.com>
2015-08-07 21:58:02 +0800

    up
    up

--- application/models/guide_model.php
+++ application/models/guide_model.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-include_once(APPPATH . 'models/base_model.php');
+include_once(APPPATH . 'models/Base_model.php');
 
-class guide_model extends base_model {	public function __construct() {		parent::__construct();		$this->table_name = 'class_guide';	}	public function get_list($params = array(), $page = 0, $count = 3) {		return parent::get_list($params, $page, $count);	}}
+class Guide_model extends Base_model {	public function __construct() {		parent::__construct();		$this->table_name = 'class_guide';	}	public function get_list($params = array(), $page = 0, $count = 3) {		return parent::get_list($params, $page, $count);	}}
