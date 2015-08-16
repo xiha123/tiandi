449be26d3e367ff72823c6c8fa638973264de55b
PoppinL <poppinlp@gmail.com>
2015-08-17 01:08:13 +0800

    simple search
    simple search

--- application/controllers/Seacher.php
+++ application/controllers/Seacher.php
@@ -8,10 +8,15 @@ class Seacher extends CI_Controller {
 	}
 
 	public function index() {
-		$key = $this->input->get("key");
-		$this->me['data'] = array();
-    		$this->load->view('miaoda/search.php',$this->me);
-	}
+		$this->load->model('problem_model');
+		$this->load->model('problem_detail_model');
 
+		$key = $this->input->get("key");
+		$count = 20; // 需要获取多少个
+		$result = array_merge($this->problem_model->search($key, $count), $this->problem_detail_model->search($key, $count));
+		$result = array_slice($result, 0, $count);
+		$this->me['data'] = array();
+		$this->load->view('miaoda/search.php',$this->me);
+	}
 
 }
