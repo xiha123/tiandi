3d836735b0d475f00082fe44c54c24b9fcda68f9
PoppinL <poppinlp@gmail.com>
2015-09-30 14:11:57 +0800

    bugfix

--- application/controllers/GodHelp.php
+++ application/controllers/GodHelp.php
@@ -4,10 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class godHelp extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
+		$this->me = $this->user_model->check_login();
 	}
 
 	public function index()
 	{
-		$this->load->view("help/godHelp.php");
+		$this->load->view("help/godHelp.php", $this->me);
 	}
 }
