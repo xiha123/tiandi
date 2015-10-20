9e9cd4ca9f42fea9ad038ec72db34566cfa5b6f4
xinghuo <umyba@qq.com>
2015-10-20 21:01:13 +0800

    update

--- application/views/help/gift.php
+++ application/views/help/gift.php
@@ -1,17 +1,61 @@
 <?php $this->load->view('widgets/header.php'); ?>
 <link rel="stylesheet" href="static/css/QA/formOne.css" />
+<link rel="stylesheet" href="static/css/gift.css" />
 
 </head>
 
 <body class="index">
 <?php $this->load->view('widgets/miaoda/nav.php' , []); ?>
 <?php $this->load->view('widgets/windows.php' ); ?>
-<div class="" style="margin-top: -30px;margin-bottom:60px; width: 100%;position:relative;overflow: hidden;background-position: center;background-repeat: no-repeat;background-attachment: fixed;">
+<div class="" style="margin-top: -30px; width: 100%;position:relative;overflow: hidden;background-position: center;background-repeat: no-repeat;background-attachment: fixed;">
     <img src="/static/image/gift.jpg?1444547764" width="100%" >
 </div>
-<div class="" style="margin-top: -30px; width: 100%;position:relative;overflow: hidden;background-position: center;background-repeat: no-repeat;background-attachment: fixed;">
-    <img src="/static/image/giftlist.jpg?1444547764" width="100%" >
+<div class="ad-layout c" style="margin-top: -60px;margin-bottom: 40px;">
+    <div class="ad-left">
+        <p class="ad-txt1">邀请32个大神好友</p>
+        <p class="ad-txt2">这台ipad就是你的</p>
+        <P><a href="/share/invite" target="_blank"><img src="/static/image/ad-b1.jpg" /></a></P>
+        <P class="p-line"><img src="/static/image/p-line.png" /></P>
+        <P><a href="/godHelp#what-8" target="_blank"><img src="/static/image/ad-b2.jpg" /></a></P>
+    </div>
+    <div class="ad-right">
+        <div class="ad-list">
+            <li>
+                <P class="txt1"><a>攒够100威望点就可以换<strong>程序员马克杯</strong><span class="tips"><img src="static/image/good/1-cup.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需50威望点）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够150威望点就可以换<strong>《算法帝国》/《编程之美》</strong><span class="tips"><img src="static/image/good/2-book.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需100威望点）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够300威望点就可以换<strong>小米手环</strong><span class="tips"><img src="static/image/good/3-band.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需200威望点）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够500威望点就可以换<strong>无线鼠标</strong>，每月限额15个<span class="tips"><img src="static/image/good/4-mouse.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需300威望点，且不限数量）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够900威望点就可以换<strong>机械键盘</strong>，每月限额8个<span class="tips"><img src="static/image/good/5-keyboard.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需500威望点，限额16个）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够1700威望点就可以换<strong>kindle</strong>，每月限额5个<span class="tips"><img src="static/image/good/6-kindle.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需800威望点，限额10个）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够3300威望点就可以换<strong>魔声耳机</strong>，每月限额2个<span class="tips"><img src="static/image/good/7-beats.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需1000威望点，限额5个）</P>
+            </li>
+            <li>
+                <P class="txt1"><a>攒够5500威望点就可以换<strong>iPad mini 3</strong>，每月限额1个<span class="tips"><img src="static/image/good/8-ipad¡.png" width="110" height="100" /></span></a></P>
+                <P class="txt2">（10月份仅需1280威望点，限额3个）</P>
+            </li>
+        </div>
+    </div>
 </div>
+
 <p style="text-align: right; color: #219ba1;font-size: 18px;margin-right: 100px;">*秒答保留规则制定权和解释权</p>
 <!--</div>-->
 <!--<div class="wrapper">-->
