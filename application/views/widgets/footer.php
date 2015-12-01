2489e7db3fb22d86b4c462fc4c72df4ce718f59b
12134791@qq.com <12134791>
2015-12-01 12:11:36 +0800

    pd

--- application/views/widgets/footer.php
+++ application/views/widgets/footer.php
@@ -27,7 +27,7 @@
 <script src="static/js/global.js"></script>
 
 <?php if (!isset($id)) { ?>
-<script src="http://qzonestyle.gtimg.cn/qzone/openapi/qc_loader.js" data-appid="101242237" data-redirecturi="http://www.tiandipeixun.com/qq_cb" charset="utf-8"></script>
+<script src="http://qzonestyle.gtimg.cn/qzone/openapi/qc_loader.js" data-appid="101242237" data-redirecturi="http://www.91miaoda.com/qq_cb" charset="utf-8"></script>
 <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=665902895" type="text/javascript" charset="utf-8"></script>
 <script>
 // 微博登录
